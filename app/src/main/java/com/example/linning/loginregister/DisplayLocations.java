16e052f21085a2fde00d3cc59b009f102e3053b6
lchow1 <lchow@ucsd.edu>
2015-12-04 19:23:12 -0800

    removing whitespace

--- app/src/main/java/com/example/linning/loginregister/DisplayLocations.java
+++ app/src/main/java/com/example/linning/loginregister/DisplayLocations.java
@@ -1,6 +1,6 @@
 package com.example.linning.loginregister;
 
-        import android.app.ProgressDialog;
+import android.app.ProgressDialog;
         import android.content.Context;
         import android.os.AsyncTask;
 
